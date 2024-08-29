ba4ab37d1d76c706ca0446ddf261685529b59301
OsmarAmaral <135924752+OsmarAmaral@users.noreply.github.com>
2024-08-29 11:55:13 -0400

    adicionado addNumber nos buttons

---
+++ script.js
@@ -0,0 +1,51 @@
+var number = 0
+
+function addNumber1() {
+    number = number + 1;
+    alert(number)
+}
+
+function addNumber2() {
+    number = number + 2;
+    alert(number)
+}
+
+function addNumber3() {
+    number = number + 3;
+    alert(number)
+}
+
+function addNumber4() {
+    number = number + 4;
+    alert(number)
+}
+
+function addNumber5() {
+    number = number + 5;
+    alert(number)
+}
+
+function addNumber6() {
+    number = number + 6;
+    alert(number)
+}
+
+function addNumber7() {
+    number = number + 7;
+    alert(number)
+}
+
+function addNumber8() {
+    number = number + 8;
+    alert(number)
+}
+
+function addNumber9() {
+    number = number + 9;
+    alert(number)
+}
+
+function addNumber0() {
+    number = number + 0;
+    alert(number)
+}
